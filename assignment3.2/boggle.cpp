b44023ac9c134d088e63ea3b46f367b2e8d135aa
tgreen443 <60002376+tgreen443@users.noreply.github.com>
2022-10-20 15:51:40 +0800

    Add files via upload

---
+++ assignment3.2/boggle.cpp
@@ -0,0 +1,178 @@
+/*
+ * TODO: remove and replace this file header comment
+ * You will edit and turn in this file.
+ * Remove starter comments and add your own
+ * comments on each function and on complex code sections.
+ */
+#include <iostream>    // for cout, endl
+#include <string>      // for string class
+#include "backtracking.h"
+#include "gridlocation.h"
+#include "grid.h"
+#include "set.h"
+#include "lexicon.h"
+#include "testing/SimpleTest.h"
+using namespace std;
+
+/*
+ * TODO: Replace this comment with a descriptive function
+ * header comment.
+ */
+int points(string str) {
+    int len=str.size();
+    if(len<4){
+        return 0;
+    }
+    else{
+        return (len-3);
+    }
+    return 0;
+}
+
+/*
+ * TODO: Replace this comment with a descriptive function
+ * header comment.
+ */
+Set<GridLocation> generateNeighbors (Grid<char> & board,GridLocation &loc,Set<GridLocation> visitedPoints){
+    Set<GridLocation> neighbors;
+    int i= loc.row;
+    int j= loc.col;
+    for(int m=i-1;m<=i+1;m++){
+        for(int n=j-1;n<=j+1;n++){
+            GridLocation temp(m,n);
+            if(board.inBounds(temp)&&(!visitedPoints.contains(temp))&&isalpha(board[temp])&&temp!=loc){
+                    neighbors.add(temp);
+            }
+        }
+    }
+    return neighbors;
+}
+void scoreBoardRec(Grid<char>& board, Lexicon& lex,string wordStack,Stack<GridLocation> &locStack,GridLocation loc,Set <GridLocation> visitedPoints, Set<GridLocation> neighbors,Set<string> &words){
+    visitedPoints.add(loc);
+    wordStack+=board[loc];
+    locStack.push(loc);    
+    if(!isalpha(board[loc])){
+        return;
+    }
+    //basecase
+    if(neighbors.isEmpty()){
+        if(lex.contains(wordStack)){
+            words.add(wordStack);
+        }
+        return;
+    }
+    else{
+        //递归其中一个点
+        GridLocation nextLoc=neighbors.last();
+        auto nextNeighbors=generateNeighbors (board,nextLoc,visitedPoints);
+        scoreBoardRec(board,lex,wordStack,locStack,nextLoc,visitedPoints,nextNeighbors,words);
+        //或者平行的其他点
+        neighbors.remove(nextLoc);
+        wordStack.pop_back();
+        scoreBoardRec(board,lex,wordStack,locStack,loc,visitedPoints,neighbors,words);
+    }
+}
+int scoreBoard(Grid<char>& board,Lexicon &lex) {
+    int row= board.numRows();
+    int col= board.numCols();
+    int score =0;
+
+    Set<string> words;
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            Set<GridLocation> visitedPoints;
+            GridLocation loc(i,j);
+            string wordStack;
+
+            Stack<GridLocation> locStack;
+            auto neighbors=generateNeighbors(board,loc,visitedPoints);
+            scoreBoardRec(board,lex,wordStack,locStack,loc,visitedPoints,neighbors,words);
+        }
+    }
+    for(string n:words){
+            score+=points(n);
+    }
+    return score;
+}
+
+/* * * * * * Test Cases * * * * * */
+
+/* Test helper function to return shared copy of Lexicon. Use to
+ * avoid (expensive) re-load of word list on each test case. */
+static Lexicon& sharedLexicon() {
+    static Lexicon lex("res/EnglishWords.txt");
+    return lex;
+}
+PROVIDED_TEST("Load shared Lexicon, confirm number of words") {
+    Lexicon lex = sharedLexicon();
+    EXPECT_EQUAL(lex.size(), 127145);
+}
+
+PROVIDED_TEST("Test point scoring") {
+    EXPECT_EQUAL(points("and"), 0);
+    EXPECT_EQUAL(points("quad"), 1);
+    EXPECT_EQUAL(points("quint"), 2);
+    EXPECT_EQUAL(points("sextet"), 3);
+    EXPECT_EQUAL(points("seventh"), 4);
+    EXPECT_EQUAL(points("supercomputer"), 10);
+}
+PROVIDED_TEST("Test scoreBoard, board contains no words, score of zero") {
+    Grid<char> board = {{'B','C','D','F'}, //no vowels, no words
+                        {'G','H','J','K'},
+                        {'L','M','N','P'},
+                        {'Q','R','S','T'}};
+    EXPECT_EQUAL(scoreBoard(board, sharedLexicon()), 0);
+}
+
+PROVIDED_TEST("Test scoreBoard, board contains one word, score of 1") {
+    Grid<char> board = {{'C','_','_','_'},
+                        {'Z','_','_','_'},
+                        {'_','A','_','_'},
+                        {'_','_','R','_'}};
+    EXPECT_EQUAL(scoreBoard(board, sharedLexicon()), 1);
+}
+PROVIDED_TEST("Test scoreBoard, alternate paths for same word, still score of 1") {
+    Grid<char> board = {{'C','C','_','_'},
+                        {'C','Z','C','_'},
+                        {'_','A','_','_'},
+                        {'R','_','R','_'}};
+    EXPECT_EQUAL(scoreBoard(board, sharedLexicon()), 1);
+}
+
+PROVIDED_TEST("Test scoreBoard, small number of words in corner of board") {
+    Grid<char> board = {{'L','I','_','_'},
+                        {'M','E','_','_'},
+                        {'_','S','_','_'},
+                        {'_','_','_','_'}};
+    Set<string> words = {"SMILE", "LIMES", "MILES", "MILE", "MIES", "LIME", "LIES", "ELMS", "SEMI"};
+
+    EXPECT_EQUAL(scoreBoard(board, sharedLexicon()),  2 + 2 + 2 + 1 + 1 + 1 + 1 + 1 + 1);
+}
+
+PROVIDED_TEST("Test scoreBoard, full board, small number of words") {
+    Grid<char> board = {{'E','Z','R','R'},
+                        {'O','H','I','O'},
+                        {'N','J','I','H'},
+                        {'Y','A','H','O'}};
+    Set<string> words = { "HORIZON", "OHIA", "ORZO", "JOHN", "HAJI"};
+
+    EXPECT_EQUAL(scoreBoard(board, sharedLexicon()), 4 + 1 + 1 + 1 + 1);
+}
+
+PROVIDED_TEST("Test scoreBoard, full board, medium number of words") {
+    Grid<char> board = {{'O','T','H','X'},
+                        {'T','H','T','P'},
+                        {'S','S','F','E'},
+                        {'N','A','L','T'}};
+
+    EXPECT_EQUAL(scoreBoard(board, sharedLexicon()), 76);
+}
+
+PROVIDED_TEST("Test scoreBoard, full board, large number of words") {
+    Grid<char> board = {{'E','A','A','R'},
+                        {'L','V','T','S'},
+                        {'R','A','A','N'},
+                        {'O','I','S','E'}};
+
+    EXPECT_EQUAL(scoreBoard(board, sharedLexicon()), 234);
+}
---
+++ assignment3.2/voting.cpp
@@ -0,0 +1,124 @@
+// TODO: remove and replace this file header comment
+// This is a .cpp file you will edit and turn in.
+// Remove starter comments and add your own
+// comments on each function and on complex code sections.
+#include <iostream>    // for cout, endl
+#include <string>      // for string class
+#include "voting.h"
+
+#include "testing/SimpleTest.h"
+using namespace std;
+
+// TODO: Add a function header comment here to explain the
+// behavior of the function and how you implemented this behavior
+
+int blocksSum(Vector<int>&blocks){
+    int sum=0;
+    for (int n:blocks){
+        sum+=n;
+    }
+    return sum;
+}
+void subBlocksRec(Vector<int> &blocks,int head,Vector<int> soFar,const int &winSum,Vector<int> &preResult,int soFarSum,Vector<int> &leastWinNum){
+    if(head==blocks.size()){
+        if(soFarSum>=winSum){
+            for(auto n:soFar){
+                //如果除去这个区域选票失败
+                if((soFarSum-blocks[n])<winSum){
+                    preResult[n]++;
+                }
+            }
+
+        }
+        return;
+    }
+    else{
+        //判断是否是无效分支
+        if(soFarSum<leastWinNum[head]){
+            return;
+        }
+        //递归
+        subBlocksRec(blocks,head+1,soFar,winSum,preResult,soFarSum,leastWinNum);
+        //添加
+        soFar.add(head);
+        //递归
+        subBlocksRec(blocks,head+1,soFar,winSum,preResult,soFarSum+blocks[head],leastWinNum);
+    }
+}
+Vector<int> subBlocks(Vector<int> &blocks){
+    int head=0;
+    int allSum=blocksSum(blocks);
+    int winSum =allSum/2+1;
+    Vector<int> preResult(blocks.size(),0);
+    Vector<int> soFar;
+    Vector<int> leastWinNum(blocks.size(),0);
+    for(int i=0;i<blocks.size();i++){
+        int tempNum=0;
+        for(int j=i;j<blocks.size();j++){
+            tempNum+=blocks[j];
+        }
+        leastWinNum[i]=winSum-tempNum;
+    }
+    int soFarSum=0;
+    subBlocksRec(blocks,head,soFar,winSum,preResult,soFarSum,leastWinNum);
+    return preResult;
+}
+Vector<int> computePowerIndexes(Vector<int>&blocks)
+{
+    //取出所有子集
+    Vector<int> preResult=subBlocks(blocks);
+    Vector<int> result(preResult.size(),0);
+    int winCounts = blocksSum(preResult);
+    for(int i=0;i<preResult.size();i++){
+        if(i>0&&blocks[i]==blocks[i-1]){
+            result[i]=result[i-1];
+        }
+        else{
+            result[i]=(preResult[i]*100)/winCounts;
+        }
+    }
+    return result;
+}
+
+/* * * * * * Test Cases * * * * * */
+
+PROVIDED_TEST("Test power index, blocks 50-49-1") {
+    Vector<int> blocks = {50, 49, 1};
+    Vector<int> expected = {60, 20, 20};
+    EXPECT_EQUAL(computePowerIndexes(blocks), expected);
+}
+
+PROVIDED_TEST("Test power index, blocks Hempshead 1-1-3-7-9-9") {
+    Vector<int> blocks = {1, 1, 3, 7, 9, 9};
+    Vector<int> expected = {0, 0, 0, 33, 33, 33};
+    EXPECT_EQUAL(computePowerIndexes(blocks), expected);
+}
+
+PROVIDED_TEST("Test power index, blocks CA-TX-NY 55-38-39") {
+    Vector<int> blocks = {55, 38, 29};
+    Vector<int> expected = {33, 33, 33};
+    EXPECT_EQUAL(computePowerIndexes(blocks), expected);
+}
+
+PROVIDED_TEST("Test power index, blocks CA-TX-GA 55-38-16") {
+    Vector<int> blocks = {55, 38, 16};
+    Vector<int> expected = {100, 0, 0};
+    EXPECT_EQUAL(computePowerIndexes(blocks), expected);
+}
+
+PROVIDED_TEST("Test power index, blocks EU post-Nice") {
+    // Estonia is one of those 4s!!
+    Vector<int> blocks = {29,29,29,29,27,27,14,13,12,12,12,12,12,10,10,10,7,7,7,7,7,4,4,4,4,4,3};
+    Vector<int> expected = {8, 8, 8, 8, 7, 7, 3, 3, 3, 3, 3, 3, 3, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0};
+    EXPECT_EQUAL(computePowerIndexes(blocks), expected);
+}
+
+PROVIDED_TEST("Time power index operation") {
+    Vector<int> blocks;
+    for (int i = 0; i < 15; i++) {
+        blocks.add(randomInteger(1, 10));
+    }
+    TIME_OPERATION(blocks.size(), computePowerIndexes(blocks));
+}
+
+
